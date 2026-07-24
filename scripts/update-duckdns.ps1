ac769c78b1a14942781dd4e4db9f0c1cc6ffc5dd
Rum <rum.netspace@gmail.com>
2026-07-24 10:23:36 +0700

    add script to sync dynamic public ip to duckdns

---
+++ scripts/update-duckdns.ps1
@@ -0,0 +1,10 @@
+﻿$domain = $env:DUCKDNS_DOMAIN
+$token = $env:DUCKDNS_PRIVATE_TOKEN
+
+# Kiểm tra nếu chưa set biến môi trường thì báo lỗi
+if ([string]::IsNullOrEmpty($domain) -or [string]::IsNullOrEmpty($token)) {
+    Write-Error "Chưa cấu hình biến môi trường DUCKDNS_DOMAIN hoặc DUCKDNS_TOKEN!"
+    exit 1
+}
+
+Invoke-RestMethod -Uri "https://www.duckdns.org/update?domains=$domain&token=$token&ip="
